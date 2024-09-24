a3a51c1e86c173477008b1e636301525b76a0205
farzin jimenez <farzinjimenez92@gmail.com>
2024-09-24 09:14:38 -0500

    ru

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { ContactFormComponent } from '@features/contacts/contact-form/contact-form.component';
 
 export const routes: Routes = [
-
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
   { path: 'contacts', loadChildren: () => import('./features/contacts/contacts.routes') },
   { path: 'contact-form', component: ContactFormComponent },  // Nueva ruta para el formulario
-
+  { path: '**', redirectTo: '/contacts' }
 ];
